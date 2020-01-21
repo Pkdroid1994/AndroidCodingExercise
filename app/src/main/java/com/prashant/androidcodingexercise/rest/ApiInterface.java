e9a4cca3b53e91eca73d716e5332f1ea5e252bd0
Pk <prashant.kumar@agicent.com>
2020-01-21 18:25:47 +0530

    Added New Test Cases

--- app/src/main/java/com/prashant/androidcodingexercise/rest/ApiInterface.java
+++ app/src/main/java/com/prashant/androidcodingexercise/rest/ApiInterface.java
@@ -3,12 +3,13 @@ package com.prashant.androidcodingexercise.rest;
 
 
 import com.prashant.androidcodingexercise.data.AllFactsResponseModel;
+import com.prashant.androidcodingexercise.utils.ConstantsUtil;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
 
-    @GET("s/2iodh4vg0eortkl/facts.json")
+    @GET(ConstantsUtil.FACTS_JSON_KEY)
     Observable<AllFactsResponseModel> getFactsList();
 }
--- app/src/main/java/com/prashant/androidcodingexercise/utils/ConstantsUtil.java
+++ app/src/main/java/com/prashant/androidcodingexercise/utils/ConstantsUtil.java
@@ -2,4 +2,5 @@ package com.prashant.androidcodingexercise.utils;
 
 public interface ConstantsUtil {
     String BASE_URL="https://dl.dropboxusercontent.com/";
+    String FACTS_JSON_KEY="s/2iodh4vg0eortkl/facts.json";
 }
